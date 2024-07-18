16477a197a6b8af7c10dcee80b9d214e74e3ea22
krisna <krisnamadani@gmail.com>
2024-07-18 14:18:33 +0700

    universal relation loading trait

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -4,25 +4,22 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\EventResource;
+use App\Http\Traits\CanLoadRelationships;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
+    use CanLoadRelationships;
+
+    private array $relations = ['user', 'attendees', 'attendees.user'];
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        $query = Event::query();
-        $relations = ['user', 'attendees', 'attendees.user'];
-
-        foreach ($relations as $relation) {
-            $query->when(
-                $this->shouldIncludeRelation($relation),
-                fn($q) => $q->with($relation)
-            );
-        }
+        $query = $this->loadRelationships(Event::query());
         
         return EventResource::collection(
             $query->latest()->paginate()
@@ -44,7 +41,7 @@ class EventController extends Controller
             'user_id' => 1
         ]);
 
-        return new EventResource($event);
+        return new EventResource($this->loadRelationships($event));
     }
 
     /**
@@ -52,9 +49,7 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        $event->load('user', 'attendees');
-
-        return new EventResource($event);
+        return new EventResource($this->loadRelationships($event));
     }
 
     /**
@@ -71,7 +66,7 @@ class EventController extends Controller
             ])
         );
 
-        return new EventResource($event);
+       return new EventResource($this->loadRelationships($event));
     }
 
     /**
@@ -82,18 +77,5 @@ class EventController extends Controller
         $event->delete();
 
         return response(status: 204);
-    }
-
-    protected function shouldIncludeRelation(string $relation): bool
-    {
-        $include = request('include');
-
-        if (!$include) {
-            return false;
-        }
-
-        $relations = array_map('trim', explode(',', $include));
-
-        return in_array($relation, $relations);
     }
 }
